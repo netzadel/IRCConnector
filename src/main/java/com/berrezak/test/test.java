cc17b93dcbf70cdca576173cad0cb2ad76c33886
elberro <elberro@online.de>
2016-02-04 12:02:26 +0100

    added some comments
    Signed-off-by: elberro <elberro@online.de>

--- src/main/java/com/berrezak/test/test.java
+++ src/main/java/com/berrezak/test/test.java
@@ -13,17 +13,24 @@ import java.io.IOException;
 public class test implements IMessageReceiver {
 
     public void start() {
+        //Create a profile with server and user info
         IRCProfile profile = new IRCProfile("elberro", "underworld2.no.quakenet.org", 6667);
+
+        //Create an instance of ConnectionManager and open the connection to the server
         ConnectionManager manager = new ConnectionManager(profile);
         manager.openConnection();
 
+        //Create/Join a channel with the ConnectionManager object
         IRCChannel beginner = manager.connectChannel("beginner", false);
+
+        //Start interacting with the channel
         if (beginner != null) {
             beginner.registerForChatMessages(this);
             beginner.sendMessage("Hello!");
         }
     }
 
+    //Receive chat massages from users here
     @Override
     public void receivedMessage(String user, String message) {
         System.out.println("<< test class received the following from: " + user + " message: " + message);
